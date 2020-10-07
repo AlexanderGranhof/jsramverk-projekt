3e3fb1012f67f2208002ffe74279d6054547642b
Alexander Granhof <alexander.g737@gmail.com>
2020-10-07 23:19:15 +0200

    created a schema and interface for a transaction

---
+++ server/src/services/db/schemas/transaction.ts
@@ -0,0 +1,30 @@
+import { Schema, Document } from 'mongoose'
+
+export interface ITransaction extends Document {
+    trade: string
+    market: string
+    user: string
+    createdAt: Date
+}
+
+const Transaction = new Schema({
+    trade: {
+        type: String,
+        required: true,
+    },
+    market: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: String,
+        default: 'bot',
+    },
+    createdAt: Date,
+})
+
+Transaction.pre('save', function () {
+    this.set({ createdAt: new Date() })
+})
+
+export default Transaction
