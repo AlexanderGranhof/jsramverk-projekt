a91a98fe28f6a2d677576291f1df01f752cd50f4
Alexander Granhof <alexander.g737@gmail.com>
2020-10-07 23:32:50 +0200

    created intitial express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,16 @@
 import * as market from './services/market'
+import express from 'express'
+import chalk from 'chalk'
+import sockets from './services/socket'
+
+const port = process.env.PORT || 3000
+
+const app = express()
 
 market.start()
+
+const server = app.listen(port, () => {
+    console.log(chalk.yellow('[INDEX]:'), `Server is listening on ${port}`)
+})
+
+sockets(server)
