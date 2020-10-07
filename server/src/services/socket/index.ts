2da3885edc848f1f5942aae1c4e3e6032f635cc2
Alexander Granhof <alexander.g737@gmail.com>
2020-10-08 00:24:16 +0200

    initialize transactions service with the socket server

--- server/src/services/socket/index.ts
+++ server/src/services/socket/index.ts
@@ -1,10 +1,14 @@
 import SocketIO from 'socket.io'
 import chalk from 'chalk'
+import * as transactions from './transactions'
 
-export const log = (...args: any[]) => console.log(chalk.yellow(`[SOCKET]:`), ...args)
+export const log = (...args: any[]) => console.log(chalk.blueBright(`[SOCKET]:`), ...args)
 
 export default (server: Express.Application) => {
     const io = SocketIO(server)
+    log('Socket server is running')
+
+    transactions.initialize(io)
 
     io.on('connection', (socket) => {
         log('Client has connected')
