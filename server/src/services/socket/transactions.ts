66ff82688380c23308507a9341c80976052aacf5
Alexander Granhof <alexander.g737@gmail.com>
2020-10-13 17:23:25 +0200

    send market history to client

--- server/src/services/socket/transactions.ts
+++ server/src/services/socket/transactions.ts
@@ -1,9 +1,17 @@
 import { Socket, Server } from 'socket.io'
+import ohlcHandler from '../market/ohlc'
+import db from '../db'
 
 let io: Server
 
 export const initialize = (newIO: Server) => {
     io = newIO
+
+    io.on('connection', (socket) => {
+        socket.on('market_history', async (cb) => {
+            cb(ohlcHandler.ohlcs)
+        })
+    })
 }
 
 export const send = (transaction: any) => {
