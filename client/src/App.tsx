33fe037e92171490e851c580ac09ec84326d183f
Alexander Granhof <alexander.g737@gmail.com>
2020-10-13 17:33:17 +0200

    Updated app.tsx to use the d3 chart

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 import { useOHLC } from './hooks/market'
+import Chart from './components/chart'
 
 function App() {
-    useOHLC()
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { ohlc } = useOHLC()
+    const [domain, setDomain] = useState<[number, number]>([0, 0])
+
+    useEffect(() => {
+        const values = ohlc.map((transaction) => [transaction.low, transaction.high]).flat() as number[]
+
+        const min = Math.min(...values)
+        const max = Math.max(...values)
+
+        const domain = [isFinite(min) ? min : 0, isFinite(max) ? max : 0] as [number, number]
+
+        setDomain(domain)
+    }, [ohlc])
+
+    return <Chart width={window.innerWidth} height={window.innerHeight} candles={ohlc} caliber={10} domain={domain} />
 }
 
-export default App;
+export default App
