9eca90196f71e7a8e67cd332fa156176d0cd78d8
Alexander Granhof <alexander.g737@gmail.com>
2020-10-14 20:00:00 +0200

    use the new user route and init dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,27 @@
-import * as market from './services/market'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
+
+// import * as market from './services/market'
 import express from 'express'
 import chalk from 'chalk'
 import sockets from './services/socket'
+import cookieParser from 'cookie-parser'
+
+import userRoute from './api/routes/user'
 
 const port = process.env.PORT || 3000
-
 const app = express()
 
-market.start()
+app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+
+app.use('/user', userRoute)
 
 const server = app.listen(port, () => {
     console.log(chalk.green('[INDEX]:'), `Server is listening on ${port}`)
+    // market.start()
 })
 
 sockets(server)
