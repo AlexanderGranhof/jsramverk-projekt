3e18a138996ee908b79ea9c0cb35c3ef4cc718df
Alexander Granhof <alexander.g737@gmail.com>
2020-10-14 19:57:08 +0200

    moved db modles into their own file

---
+++ server/src/models/index.ts
@@ -0,0 +1,18 @@
+import joi from 'joi'
+
+export type Transaction = {
+    trade: string
+    market: string
+    user: string
+    createdAt: Date
+}
+
+export type User = {
+    name: string
+    password: string
+}
+
+export const UserSchema = joi.object({
+    name: joi.string().required(),
+    password: joi.string().required(),
+})
