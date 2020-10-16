4a9cb3ef7948f47fcdd850edc7077994d91f6873
Alexander Granhof <alexander.g737@gmail.com>
2020-10-16 02:21:06 +0200

    created a fetch service

---
+++ client/src/services/fetch.ts
@@ -0,0 +1,20 @@
+const isProduction = window.location.hostname !== 'localhost'
+
+const hostname = window.location.hostname
+const protocol = window.location.protocol
+const subdomain = isProduction ? 'me-api.' : ''
+const port = isProduction ? (window.location.port ? ':' + window.location.port : '') : ':3001'
+
+const host = `${protocol}//${subdomain}${hostname}${port}`
+
+export default (path = '', body?: Record<string, any>) => {
+    const url = path.startsWith('/') ? host + path : `${host}/${path}`
+    const headers = body ? { 'Content-Type': 'application/json' } : undefined
+
+    return fetch(url, {
+        method: body ? 'POST' : 'GET',
+        credentials: 'include',
+        body: body ? JSON.stringify(body) : undefined,
+        headers,
+    })
+}
