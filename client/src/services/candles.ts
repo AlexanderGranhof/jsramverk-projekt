abc4230ae29d93e556ff9a00b0bdffcb7e40a2b3
Alexander Granhof <alexander.g737@gmail.com>
2020-10-16 02:21:27 +0200

    made candles service into a promise

--- client/src/services/candles.ts
+++ client/src/services/candles.ts
@@ -1,62 +1,64 @@
 import dayjs, { OpUnitType } from 'dayjs'
 import { Candle as CandleModel } from '../models/market'
 
-export const squashCandles = (candles: any, value: number, unit: OpUnitType): CandleModel[] => {
-    const ohlcs = candles.reduce(
-        (acc: any[], candle: any) => {
-            const prev = acc.pop()
+export const squashCandles = (candles: any, value: number, unit: OpUnitType): Promise<CandleModel[]> => {
+    return new Promise((resolve) => {
+        const ohlcs = candles.reduce(
+            (acc: any[], candle: any) => {
+                const prev = acc.pop()
 
-            if (prev.merged) {
-                return [...acc, prev, { open: prev.close }]
-            }
+                if (prev.merged) {
+                    return [...acc, prev, { open: prev.close }]
+                }
 
-            if (!prev.open) {
-                prev.open = candle.open
-            }
+                if (!prev.open) {
+                    prev.open = candle.open
+                }
 
-            if (!prev.high) {
-                prev.high = candle.high
-            }
+                if (!prev.high) {
+                    prev.high = candle.high
+                }
 
-            if (!prev.low) {
-                prev.low = candle.low
-            }
+                if (!prev.low) {
+                    prev.low = candle.low
+                }
 
-            if (!prev.createdAt) {
-                prev.createdAt = candle.createdAt
-            }
+                if (!prev.createdAt) {
+                    prev.createdAt = candle.createdAt
+                }
 
-            const newOHLC = { ...prev }
+                const newOHLC = { ...prev }
 
-            if (candle.high > newOHLC.high) {
-                newOHLC.high = candle.high
-            }
+                if (candle.high > newOHLC.high) {
+                    newOHLC.high = candle.high
+                }
 
-            if (candle.low < newOHLC.low) {
-                newOHLC.low = candle.low
-            }
+                if (candle.low < newOHLC.low) {
+                    newOHLC.low = candle.low
+                }
 
-            const diff = dayjs(candle.createdAt).diff(dayjs(newOHLC.createdAt), unit)
+                const diff = dayjs(candle.createdAt).diff(dayjs(newOHLC.createdAt), unit)
 
-            if (diff >= value) {
-                return [
-                    ...acc,
-                    {
-                        ...newOHLC,
-                        close: candle.close,
-                        merged: true,
-                    },
-                ]
-            }
+                if (diff >= value) {
+                    return [
+                        ...acc,
+                        {
+                            ...newOHLC,
+                            close: candle.close,
+                            merged: true,
+                        },
+                    ]
+                }
 
-            return [...acc, newOHLC]
-        },
-        [{}],
-    )
+                return [...acc, newOHLC]
+            },
+            [{}],
+        )
 
-    if (!ohlcs[ohlcs.length - 1].close) {
-        return ohlcs.slice(0, -1)
-    }
+        if (!ohlcs[ohlcs.length - 1].close) {
+            return ohlcs.slice(0, -1)
+        }
 
-    return ohlcs
+        resolve(ohlcs)
+    })
 }
