e26d55c3e2b4a8ae9c84dffa0ee0d63154f78fe5
Alexander Granhof <alexander.g737@gmail.com>
2020-10-16 02:23:36 +0200

    use cors and morgan

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,25 +3,31 @@ import path from 'path'
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
-// import * as market from './services/market'
+import * as market from './services/market'
 import express from 'express'
 import chalk from 'chalk'
 import sockets from './services/socket'
 import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import cors from 'cors'
 
 import userRoute from './api/routes/user'
 
+const isProd = process.env.NODE_ENV === 'production'
 const port = process.env.PORT || 3000
+const loggingMode = isProd ? 'combined' : 'dev'
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
+app.use(morgan(loggingMode))
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 
 app.use('/user', userRoute)
 
 const server = app.listen(port, () => {
     console.log(chalk.green('[INDEX]:'), `Server is listening on ${port}`)
-    // market.start()
+    market.start()
 })
 
 sockets(server)
