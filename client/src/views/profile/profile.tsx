dfb61d8e37ae8cfeec672f37ff8a460abc67889c
Alexander Granhof <alexander.g737@gmail.com>
2020-10-16 02:21:53 +0200

    created basic profile view

---
+++ client/src/views/profile/profile.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react'
+
+const Profile: FunctionComponent = () => {
+    return (
+        <div>
+            <h1>Profile</h1>
+        </div>
+    )
+}
+
+export default Profile
