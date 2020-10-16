0b7358bbe54fc16684669a7cb888b0d4004fd73e
Alexander Granhof <alexander.g737@gmail.com>
2020-10-16 17:15:53 +0200

    added a add balance method

--- client/src/services/user.ts
+++ client/src/services/user.ts
@@ -18,9 +18,15 @@ export const validate = async () => {
     const response = await apiFetch('/user/validate/cookie')
     const data = await response.json()
 
-    return data.valid === true
+    return [response, data]
 }
 
 export const logout = () => {
     return apiFetch('/user/signout', {})
+}
+
+export const addBalance = (amount: number) => {
+    return apiFetch('/user/balance', {
+        add: amount,
+    })
 }
