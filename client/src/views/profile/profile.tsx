2443aa8f47bc64c179b2e644f9e4769f4672af67
Alexander Granhof <alexander.g737@gmail.com>
2020-10-16 17:15:41 +0200

    Added balance and add balance to profile view

--- client/src/views/profile/profile.tsx
+++ client/src/views/profile/profile.tsx
@@ -1,9 +1,55 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useContext, useState, useEffect, useRef } from 'react'
+import styles from './profile.module.scss'
+import { userContext, UserContext } from '../../context/user'
+import { Button, message } from 'antd'
+import gsap from 'gsap'
+import * as user from '../../services/user'
 
 const Profile: FunctionComponent = () => {
+    const [userState, setUserState] = useContext<UserContext>(userContext)
+    const containerRef = useRef(null)
+
+    const handleAddBalance = async () => {
+        try {
+            const response = await user.addBalance(10000)
+            const data = await response.json()
+
+            setUserState({
+                ...userState,
+                balance: data.balance,
+            })
+            message.success('Added $10,000 to your account')
+        } catch {
+            message.error('Unable to add balance to user account')
+        }
+    }
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+    })
+
+    useEffect(() => {
+        if (!containerRef.current) return
+
+        gsap.from(containerRef.current, { y: 50, opacity: 0, visibility: 'visible' })
+        gsap.to(containerRef.current, { y: 0, opacity: 1, duration: 0.5, ease: 'power3.out', visibility: 'visible' })
+    }, [containerRef])
+
     return (
-        <div>
-            <h1>Profile</h1>
+        <div style={{ visibility: 'hidden' }} ref={containerRef}>
+            <h1 className={styles['name']}>{userState.name}</h1>
+
+            <div className={styles['balance-container']}>
+                <p>Balance</p>
+
+                <h1 className={styles['current-balance']}>{formatter.format(userState.balance)}</h1>
+
+                <Button onClick={handleAddBalance} type="primary">
+                    Add balance
+                </Button>
+            </div>
         </div>
     )
 }
