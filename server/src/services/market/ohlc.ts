f56fea29b44a5902453d27031970401292e6f038
Alexander Granhof <alexander.g737@gmail.com>
2020-10-18 23:42:05 +0200

    added new squash method for ohlcs

--- server/src/services/market/ohlc.ts
+++ server/src/services/market/ohlc.ts
@@ -1,4 +1,4 @@
-import dayjs from 'dayjs'
+import dayjs, { OpUnitType } from 'dayjs'
 import { Transaction } from '../../models'
 
 type OHLC = {
@@ -31,6 +31,66 @@ class OHLCHandler {
 
     lastOHLC() {
         return this.ohlcs[this.ohlcs.length - 1]
+    }
+
+    squashTransactions(value: number, unit: OpUnitType) {
+        const ohlcs = this.ohlcs.reduce(
+            (acc: any[], candle) => {
+                const prev = acc.pop()
+
+                if (prev.merged) {
+                    return [...acc, prev, { open: prev.close }]
+                }
+
+                if (!prev.open) {
+                    prev.open = candle.open
+                }
+
+                if (!prev.high) {
+                    prev.high = candle.high
+                }
+
+                if (!prev.low) {
+                    prev.low = candle.low
+                }
+
+                if (!prev.createdAt) {
+                    prev.createdAt = candle.createdAt
+                }
+
+                const newOHLC = { ...prev }
+
+                if (candle.high > newOHLC.high) {
+                    newOHLC.high = candle.high
+                }
+
+                if (candle.low < newOHLC.low) {
+                    newOHLC.low = candle.low
+                }
+
+                const diff = dayjs(candle.createdAt).diff(dayjs(newOHLC.createdAt), unit)
+
+                if (diff >= value) {
+                    return [
+                        ...acc,
+                        {
+                            ...newOHLC,
+                            close: candle.close,
+                            merged: true,
+                        },
+                    ]
+                }
+
+                return [...acc, newOHLC]
+            },
+            [{}],
+        )
+
+        if (!ohlcs[ohlcs.length - 1].close) {
+            return ohlcs.slice(0, -1)
+        }
+
+        return ohlcs
     }
 
     private processTransactions(transactions: Transaction[]) {
