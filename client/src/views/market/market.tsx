d0dc8c957f3327e31ded7978ff105a34030efd80
Alexander Granhof <alexander.g737@gmail.com>
2020-10-22 14:02:38 +0200

    added live candles and live adjustment

--- client/src/views/market/market.tsx
+++ client/src/views/market/market.tsx
@@ -4,12 +4,20 @@ import { useState, useEffect } from 'react'
 import Chart from '../../components/chart'
 import gsap from 'gsap'
 import { LoadingOutlined } from '@ant-design/icons'
+import socket from '../../services/socket'
+import { OpUnitType } from 'dayjs'
+import { createCandles } from '../../services/candles'
+import { Switch } from 'antd'
 
 const Market: FunctionComponent = () => {
     const { ohlc } = useOHLC()
     const [domain, setDomain] = useState<[number, number]>([0, 0])
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(false)
+    const [latestTransactions, setLatestTransactions] = useState<any>([])
+    const [latestOHLC, setLatestOHLC] = useState<any>([])
+    const [chartScale, setChartScale] = useState<[number, OpUnitType]>([5, 'second'])
     const containerRef = useRef(null)
+    const [autoScroll, setAutoScroll] = useState(true)
 
     const contentMargin = 50
 
@@ -36,22 +44,58 @@ const Market: FunctionComponent = () => {
         setDomain(domain)
     }, [ohlc])
 
+    useEffect(() => {
+        const [value, unit] = chartScale
+        const [newOHLC] = createCandles(latestTransactions, value, 'year')
+
+        setLatestOHLC([newOHLC])
+    }, [latestTransactions])
+
+    const reset = () => {
+        setLatestTransactions([])
+        setLatestOHLC([])
+    }
+
+    useEffect(() => {
+        socket.on('market_transaction', (data: any) => {
+            setLatestTransactions((currentTransactions: any) => {
+                return [...currentTransactions, data]
+            })
+        })
+        return () => {
+            socket.off('market_transaction')
+        }
+    }, [])
+
+    if (loading) {
+        return (
+            <LoadingOutlined
+                style={{ fontSize: '10em', position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}
+            />
+        )
+    }
+
     return (
         <React.Fragment>
-            {loading && (
-                <LoadingOutlined
-                    style={{ fontSize: '10em', position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}
-                />
-            )}
+            <div>
+                <span>Autoscroll</span>
+                <Switch checked={autoScroll} onChange={setAutoScroll} />
+            </div>
             <div ref={containerRef} style={{ opacity: 0 }}>
                 <Chart
-                    autoScroll={true}
+                    autoScroll={autoScroll}
                     width={width}
                     height={window.innerHeight / 2}
+                    liveCandle={latestOHLC}
+                    liveCandleLastTransaction={
+                        latestTransactions.length ? latestTransactions[latestTransactions.length - 1] : null
+                    }
                     candles={ohlc}
                     caliber={10}
                     domain={domain}
                     onLoad={() => setLoading(false)}
+                    onScaleChange={setChartScale}
+                    onSquash={reset}
                 />
             </div>
         </React.Fragment>
