a63373eb7bac8b068a1063a7c12484509df65c82
Alexander Granhof <alexander.g737@gmail.com>
2020-10-25 18:32:57 +0100

    set jwt in docker and print jwt if its incorrect

--- server/src/services/jwt.ts
+++ server/src/services/jwt.ts
@@ -6,6 +6,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const JWT_SECRET = process.env.JWT_SECRET || ''
 
 if (JWT_SECRET.length < 16) {
+    console.log(JWT_SECRET, JWT_SECRET.length)
     throw new Error("env 'JWT_SECRET' length is less than 16")
 }
 
